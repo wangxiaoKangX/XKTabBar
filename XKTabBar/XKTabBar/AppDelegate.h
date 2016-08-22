a347845a0f7e0db38b6fc6e04482f7a88274a948
wangxiaokang <wangxiaokang@wangxiaokangdeimac.local>
2016-08-22 16:44:30 +0800

    wxk_TabBar

---
+++ XKTabBar/XKTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XKTabBar
+//
+//  Created by wangxiaokang on 16/8/22.
+//  Copyright © 2016年 Wangxiaokang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XKTabBar/XKTabBar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XKTabBar
+//
+//  Created by wangxiaokang on 16/8/22.
+//  Copyright © 2016年 Wangxiaokang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
